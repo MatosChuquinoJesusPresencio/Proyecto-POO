a7147dac8a324d93993d82f163a687b62c42849b
megap <megap@192.168.1.3>
2024-11-30 22:53:43 -0500

    Se introdujo el main

---
+++ src/Ejecutor/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ejecutor;
+import Vista.frmBienvenida;
+/**
+ *
+ * @author megap
+ */
+public class Main {
+    public static void main(String[] args) {
+       frmBienvenida frmBienvenida = new frmBienvenida();
+       frmBienvenida.setVisible(true);
+    }
+}
